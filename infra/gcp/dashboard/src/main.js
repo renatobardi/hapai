f34e65f71750d2ef2965ab5c2c508346e6a3c230
Renato Bardi <renatobardicabral@gmail.com>
2026-04-08 19:52:09 -0300

    fix: use Svelte 5 mount() API instead of deprecated new App()

--- infra/gcp/dashboard/src/main.js
+++ infra/gcp/dashboard/src/main.js
@@ -1,4 +1,5 @@
 import './app.css'
+import { mount } from 'svelte'
 import App from './App.svelte'
-const app = new App({ target: document.getElementById('app') })
-export default app
+
+mount(App, { target: document.getElementById('app') })
